0f092dab4ec05658c60c74bf437a276e962f5069
Steven Yu <ssy38@cornell.edu>
2024-05-04 21:37:04 -0400

    Initial Commit

---
+++ PocketML-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketML-iOS
+//
+//  Created by Steven Yu on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketML-iOS/PocketML_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketML_iOSApp.swift
+//  PocketML-iOS
+//
+//  Created by Steven Yu on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketML_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
